883d527c3a426404ff44d0f77438b5049d56b0c5
lucas <lucas.r.chan@gmail.com>
2024-03-16 10:14:43 -0400

    GET request for tasks in server

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const server = "http://10.230.127.70";
+
+
 function addTaskModal() {
     $("#new-task-modal").modal("show");
 }
@@ -11,9 +14,10 @@ function cancelAddTask() {
  * @param {String} title The title of the task
  * @param {String} desc The description of the task
  * @param {Number} progress Progress of the task between 0 and 1
+ * @param {String} id The task's unique ID
  * @returns jQuery node: task card
  */
-function createTaskBubble(title, desc, progress) {
+function createTaskBubble(title, desc, progress, id) {
     // Create jQuery nodes
     const $card = $("<div></div>");
     const $title = $("<h3></h3>");
@@ -39,4 +43,37 @@ function createTaskBubble(title, desc, progress) {
     $card.append($title, $desc, $progresLabel, $progressBar);
 
     return $card;
+}
+
+/**
+ * Creates a new task in the server database
+ * @param {String} title The task title
+ * @param {String} desc The task description
+ * @param {Number} time Amount of time to complete task (in hours)
+ */
+function createNewTask(title, desc, time) {
+    $.ajax({
+        type: "POST",
+        url: server + "/newtask",
+        data: JSON.stringify({
+            title, desc, time
+        }),
+        success: res => {
+            console.log(res);
+        }
+    });
+}
+
+/**
+ * Fetches tasks from server and displays them in UI.
+ */
+function getTasks() {
+    $.ajax({
+        type: "GET",
+        url: server + "/gettask",
+        dataType: "json",
+        success: res => {
+            console.log(res)
+        }
+    });
 }
